f67f1912510c7b55016d91a80b654871262ccd5a
seraphim <122089283+seraphhh444@users.noreply.github.com>
2025-10-16 01:51:08 +0300

    desktop header, footer

--- IREV/footer.php
+++ IREV/footer.php
@@ -1,4 +1,102 @@
 <?php wp_footer(); ?>
-
+<footer>
+    <div class="footer_wrapper">
+        <section class="footer_nav">
+            <div class="footer_nav_list">
+                <span class="footer_nav_list_title">
+                    PARTNER PLATFORM
+                </span>
+                <ul class="footer_nav_list_list">
+                    <li>
+                        Partner Platform
+                    </li>
+                    <li>
+                        Lead Distribution
+                    </li>
+                    <li>
+                        Integrations
+                    </li>
+                </ul>
+            </div>
+            <div class="footer_nav_list">
+                <span class="footer_nav_list_title">
+                    SOLUTIONS
+                </span>
+                <ul class="footer_nav_list_list">
+                    <li>
+                        IGaming
+                    </li>
+                    <li>
+                        Finance
+                    </li>
+                    <li>
+                        E-commerce
+                    </li>
+                </ul>
+            </div>
+            <div class="footer_nav_list">
+                <span class="footer_nav_list_title">
+                    COMPANY
+                </span>
+                <ul class="footer_nav_list_list">
+                    <li>
+                        About us
+                    </li>
+                    <li>
+                        Careers
+                    </li>
+                    <li>
+                        Contact us
+                    </li>
+                </ul>
+            </div>
+            <div class="footer_nav_list">
+                <span class="footer_nav_list_title">
+                    RESOURCES
+                </span>
+                <ul class="footer_nav_list_list">
+                    <li>
+                        Blog
+                    </li>
+                    <li>
+                        Affiliate Marketing glossary
+                    </li>
+                </ul>
+            </div>
+            <div class="footer_nav_list">
+                <span class="footer_nav_list_title">
+                    LEGAL
+                </span>
+                <ul class="footer_nav_list_list">
+                    <li>
+                        Data Control and Processing Policy
+                    </li>
+                    <li>
+                        Terms and conditions
+                    </li>
+                    <li>
+                        Privacy policy
+                    </li>
+                    <li>
+                        Cookies policy
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <div class="footer_lower">
+            <span class="footer_lower_copyrighting">
+                Copyrighting © 2025 IREV. All rights reserved.
+            </span>
+            <div>
+                <a>LINKEDIN, </a>
+                <a>FACEBOOK, </a>
+                <a>INSTAGRAM, </a>
+                <a>X</a>
+            </div>
+        </div>
+        <img class="footer_logo" src="<?php echo esc_url(get_theme_file_uri('src/icons/IREV.svg')); ?>"
+        alt="irev" />
+    </div>
+</footer>
 </body>
 </html>
--- IREV/header.php
+++ IREV/header.php
@@ -9,4 +9,44 @@
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
+<header>
+    <div class="header_wrapper">
+        <div class="header_logo">
+            <img src="<?php echo esc_url(get_theme_file_uri('src/icons/logo.svg')); ?>"
+                 alt="irev logo"/>
+        </div>
+        <div class="header_menu">
+            <div class="header_menu_item">
+                Product
+                <img src="<?php echo esc_url(get_theme_file_uri('src/icons/miniArrow.svg')); ?>"
+                     alt="arrow"
+                />
+            </div>
+            <div class="header_menu_item">
+                Solution
+                <img src="<?php echo esc_url(get_theme_file_uri('src/icons/miniArrow.svg')); ?>"
+                     alt="arrow"
+                />
+            </div>
+            <div class="header_menu_item">
+                Pricing
+            </div>
+            <div class="header_menu_item">
+                Resources
+                <img src="<?php echo esc_url(get_theme_file_uri('src/icons/miniArrow.svg')); ?>"
+                     alt="arrow"
+                />
+            </div>
+            <div class="header_menu_item">
+                Company
+                <img src="<?php echo esc_url(get_theme_file_uri('src/icons/miniArrow.svg')); ?>"
+                     alt="arrow"
+                />
+            </div>
+        </div>
+        <button class="header_signIn">
+            Sign In
+        </button>
+    </div>
+</header>
 
