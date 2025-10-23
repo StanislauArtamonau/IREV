febdd2212ed2816356c6a84f50f381846d688b92
seraphim <122089283+seraphhh444@users.noreply.github.com>
2025-10-23 12:52:17 +0300

    partner platform 1 comp

---
+++ IREV/page-partner-platform.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template name: Partner Platform Page
+ * Template Post Type: page
+ */
+get_header(); ?>
+    <main class="home_main">
+        <?php get_template_part('template-parts/partner-platform/partner-platform-represent'); ?>
+        <?php get_template_part('template-parts/home/home-represent-popup'); ?>
+    </main>
+<?php
+get_footer(); ?>
---
+++ IREV/template-parts/partner-platform/partner-platform-represent.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Template Part: Partner Platform Represent
+ */
+?>
+
+<section class="partner_platform_represent">
+    <img
+            class="back"
+            src="<?php echo esc_url(get_theme_file_uri('src/icons/pp-represent-back.svg')); ?>"
+    />
+    <div class="represent_label">
+        <h1>partner platform</h1>
+        <span>Built for scale. Powered by IREV.</span>
+    </div>
+    <div class="home_represent_rate">
+        <div class="home_represent_rate_avatars">
+            <img
+                src="<?php echo esc_url(get_theme_file_uri('src/icons/avatar1.svg')); ?>"
+            />
+            <img
+                src="<?php echo esc_url(get_theme_file_uri('src/icons/avatar2.svg')); ?>"
+            />
+            <img
+                src="<?php echo esc_url(get_theme_file_uri('src/icons/avatar3.svg')); ?>"
+            />
+            <img
+                src="<?php echo esc_url(get_theme_file_uri('src/icons/avatar4.svg')); ?>"
+            />
+            <img
+                src="<?php echo esc_url(get_theme_file_uri('src/icons/avatar5.svg')); ?>"
+            />
+            <img
+                src="<?php echo esc_url(get_theme_file_uri('src/icons/avatar6.svg')); ?>"
+            />
+        </div>
+        <img
+            class="home_represent_rate_rating"
+            src="<?php echo esc_url(get_theme_file_uri('src/icons/stars.svg')); ?>"
+        />
+        <span>
+            5/5 from 439 affiliate business owner
+        </span>
+    </div>
+
+    <div class="home_represent_form_container">
+        <form class="home_represent_form">
+            <input
+                    type="email"
+                    placeholder="Enter e-mail"
+                    class="home_represent_form_container_input"
+            />
+            <button
+                    class="home_represent_form_container_button"
+                    type="submit">
+                REV UP
+            </button>
+        </form>
+        <span>
+                [Only 5 slots left this month]
+            </span>
+    </div>
+
+
+    <span class="represent_text_lower">All-in-one software to launch, run, and grow your affiliate program — fast.</span>
+</section>
