4c143a9aad778ce39790df8c3843e40a14c2013e
seraphim <122089283+seraphhh444@users.noreply.github.com>
2025-10-30 03:58:49 +0300

    case page

---
+++ IREV/page-case.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template name: Case Page
+ * Template Post Type: page
+ */
+get_header(); ?>
+    <main class="home_main case">
+        <?php get_template_part('template-parts/case/case-represent'); ?>
+        <?php get_template_part('template-parts/case/case-component2'); ?>
+    </main>
+<?php
+get_footer(); ?>
---
+++ IREV/template-parts/case/case-represent.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template Part: Case Represent
+ */
+?>
+
+<section class="case_represent">
+    <div class="case_represent_container">
+        <img
+                src="<?php echo esc_url(get_theme_file_uri('src/icons/caselogo.svg')); ?>"
+                class="case_logo"
+        />
+        <div class="label_wrapper">
+            <h1>iGaming Partnership Launched In Heaven</h1>
+            <span>
+            Established in 2016, OX Tech works with many iGaming operators. The company provides a platform for operation with management tools, a wide choice of state-of-the-art games, and rich features
+        </span>
+        </div>
+        <img
+                src="<?php echo esc_url(get_theme_file_uri('src/icons/caserepresentback.svg')); ?>"
+                class="case_represent_back"
+        />
+    </div>
+    <div class="lower_container">
+        <ul>
+            <li>
+                <h2>[1] OX Tech’s Objectives</h2>
+                <span>The affiliate platform that OX Tech provides to its operators needed more flexibility and upscaling. The issues arose when their pixel and postback setups failed causing high volumes of discrepancy. Additionally, a significant portion of their staff could access an entire portfolio of affiliates and offers, leading to a security concern for the operators. And that’s where IREV stepped in. Among other client’s requirements were unique statistics, in-depth player analysis, and fast development of custom features.</span>
+            </li>
+            <li>
+                <span>Previously, inaccurate tracking hindered the growth of affiliate programs and caused financial losses. Now that all OX’s operators have migrated into IREV’s Partner Platform, pulling information on players became much faster and easier, alongside accurate tracking. With access to live data, OX’s operators save funds daily by monitoring the offers and instantly pausing the unprofitable ones. To tackle the conflict of interest between the affiliates being exposed to all their managers, IREV custom-developed the hierarchy of accesses, privatising and categorising data shared among staff.</span>
+                <h2>[2] IREV’s Solutions</h2>
+            </li>
+        </ul>
+    </div>
+</section>
